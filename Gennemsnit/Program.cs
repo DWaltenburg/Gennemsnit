010718267647e131585e8f79a928c5be8a12ecac
DWaltenburg <113024423+DWaltenburg@users.noreply.github.com>
2022-09-07 15:15:26 +0200

    Add files via upload

---
+++ Gennemsnit/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] tal_val = new int[100];
+int antal_tal = 0;
+App(tal_val);
+Console.WriteLine($"Gennemsnit af indtastede tal: {Avg(tal_val, antal_tal)}");
+
+Console.ReadLine();
+
+void App(int [] arr)
+{
+    Console.WriteLine("Indtast tal:");
+    while (true)
+    {
+        string temp = Console.ReadLine();
+        if (temp == "#") { return; }
+        arr[antal_tal] = Convert.ToInt32(temp);
+        antal_tal++;
+    }
+}
+static double Avg(int[] arr, int antal)
+{
+    double sum = 0;
+    for (int i = 0; i < antal; i++)
+    {
+        sum += arr[i];
+    }
+    return sum / antal;
+}
